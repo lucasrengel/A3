27cc19d7c5aff3ff4d211927ceef596904a537c5
marcosalexper <marcosalexper@hotmail.com>
2024-05-20 20:26:25 -0300

    Criada classe ferramenta

---
+++ src/main/java/modelo/Ferramenta.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Win11
+ */
+public class Ferramenta {
+    private int id;
+    private String nome;
+    private String marca;
+    private double custo;
+
+    public Ferramenta() {
+    }
+
+    public Ferramenta(int id, String nome, String marca, double custo) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+        this.custo = custo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
+    
+    
+}
