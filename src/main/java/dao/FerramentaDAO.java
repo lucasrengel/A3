bad1a348182ef044e12bb4d4aa239c256a4a511f
marcosalexper <marcosalexper@hotmail.com>
2024-05-20 20:28:51 -0300

    Update import do sql

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -1,10 +1,10 @@
 package dao;
 
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import modelo.Ferramenta;
 
