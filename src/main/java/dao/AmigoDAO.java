1110055a6145a40ec6f3be85e39ac3ba51aac530
antoniovsegala <antoniosegala023@gmail.com>
2024-05-24 19:17:17 -0300

    atualizado conexao db amigodao

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -1,5 +1,44 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AmigoDAO {
-    
+
+    public Connection getConexao() {
+
+        Connection connection = null;
+
+        try {
+
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            String server = "localhost";
+            String database = "db_a3";
+            String url = "jdbc:mysql://" + server + ":3306/" + database;
+            String user = "root";
+            String password = "1234";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            // Testando..
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NAO CONECTADO!");
+            }
+
+            return connection;
+
+        } catch (ClassNotFoundException e) {  //Driver nao encontrado
+            System.out.println("O driver nao foi encontrado. " + e.getMessage());
+            return null;
+
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+    }
 }
