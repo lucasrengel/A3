0fd12ee8b8ca839bbafb8b8b5002896f75a7a9ed
antoniovsegala <antoniosegala023@gmail.com>
2024-05-24 19:20:56 -0300

    adicionado crud

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,6 +13,7 @@ public class AmigoDAO {
 
     public static ArrayList<Amigo> minhaLista = new ArrayList<>();
 
+    //retorna a lista de amigos
     public ArrayList getMinhaLista() {
 
         minhaLista.clear();
@@ -38,15 +40,74 @@ public class AmigoDAO {
         return minhaLista;
     }
 
-    public Connection getConexao() {
-
-        Connection connection = null;
+    //cadastra um novo amigo
+    public boolean InsertAmigoBD(Amigo objeto) {
+        String sql = "INSERT INTO tb_amigos(id,nome,telefone) VALUES(?,?,?)";
 
         try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
 
+            stmt.setInt(1, objeto.getId());
+            stmt.setString(2, objeto.getNome());
+            stmt.setString(3, objeto.getTelefone());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
+        }
+    }
+
+    //deleta um amigo pelo seu id
+    public boolean DeleteAmigoBD(int id) {
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            stmt.executeUpdate("DELETE FROM tb_amigos WHERE id = " + id);
+            stmt.close();
+
+        } catch (SQLException erro) {
+        }
+
+        return true;
+    }
+
+    //atualiza um amigo pelo seu id
+    public boolean UpdateAmigoBD(Amigo objeto) {
+
+        String sql = "UPDATE tb_amigos set nome = ? ,telefone = ? WHERE id = ?";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setString(2, objeto.getTelefone());
+            stmt.setInt(3, objeto.getId());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
+        }
+
+    }
+
+    //metodo para conectar com o banco de dados
+    public Connection getConexao() {
+
+        Connection connection = null;//instancia da conexao
+
+        try {
+            //carregando o driver jdbc
             String driver = "com.mysql.cj.jdbc.Driver";
             Class.forName(driver);
 
+            //caminho do mysql
             String server = "localhost";
             String database = "db_a3";
             String url = "jdbc:mysql://" + server + ":3306/" + database;
