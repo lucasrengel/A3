f024ab3f0884322cb23ca847106cd099930cfe1e
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-28 18:56:10 -0300

    adicionado objeto pra iniciar o programa

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,8 +1,11 @@
 
 package principal;
 
+import visao.MenuPrincipal;
+
 public class Principal {
     public static void main(String[] args) {
-        
+        MenuPrincipal objeto = new MenuPrincipal();
+        objeto.setVisible(true);
     }
 }
