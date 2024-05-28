4fad999df5fb286dd7f83c2eb67e2a2f2d55c46c
isaluch <Isadoraluch@gmail.com>
2024-05-28 19:49:51 -0300

    atualizado ferramenta design

--- src/main/java/visao/TelaFerramenta.java
+++ src/main/java/visao/TelaFerramenta.java
@@ -13,6 +13,8 @@ public class TelaFerramenta extends javax.swing.JFrame {
     /**
      * Creates new form TelaFerramenta
      */
+    private int xMouse, yMouse;
+    
     public TelaFerramenta() {
         initComponents();
     }
@@ -26,21 +28,255 @@ public class TelaFerramenta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        JPmenu = new javax.swing.JPanel();
+        JBsair = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        JBalterar = new javax.swing.JButton();
+        JTFmarca = new javax.swing.JTextField();
+        JBapagar = new javax.swing.JButton();
+        JBcadastrar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        JTFnome = new javax.swing.JTextField();
+        JTFcusto = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTableFerramenta = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        JPmenu.setBackground(new java.awt.Color(102, 153, 255));
+        JPmenu.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                JPmenuMouseDragged(evt);
+            }
+        });
+        JPmenu.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                JPmenuMousePressed(evt);
+            }
+        });
+
+        JBsair.setBackground(new java.awt.Color(255, 204, 204));
+        JBsair.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        JBsair.setForeground(new java.awt.Color(255, 255, 255));
+        JBsair.setText("X");
+        JBsair.setBorder(null);
+        JBsair.setBorderPainted(false);
+        JBsair.setContentAreaFilled(false);
+        JBsair.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JBsair.setFocusPainted(false);
+        JBsair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBsairActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setBackground(new java.awt.Color(204, 255, 204));
+        jLabel4.setFont(new java.awt.Font("Ubuntu Mono", 1, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Ferramentas");
+
+        javax.swing.GroupLayout JPmenuLayout = new javax.swing.GroupLayout(JPmenu);
+        JPmenu.setLayout(JPmenuLayout);
+        JPmenuLayout.setHorizontalGroup(
+            JPmenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, JPmenuLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(JBsair, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        JPmenuLayout.setVerticalGroup(
+            JPmenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, JPmenuLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(JPmenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JBsair, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+        );
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setForeground(new java.awt.Color(255, 255, 255));
+
+        JBalterar.setBackground(new java.awt.Color(102, 153, 255));
+        JBalterar.setFont(new java.awt.Font("Noto Sans", 1, 18)); // NOI18N
+        JBalterar.setForeground(new java.awt.Color(255, 255, 255));
+        JBalterar.setText("Alterar");
+        JBalterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBalterarActionPerformed(evt);
+            }
+        });
+
+        JTFmarca.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+
+        JBapagar.setBackground(new java.awt.Color(102, 153, 255));
+        JBapagar.setFont(new java.awt.Font("Noto Sans", 1, 18)); // NOI18N
+        JBapagar.setForeground(new java.awt.Color(255, 255, 255));
+        JBapagar.setText("Apagar");
+        JBapagar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBapagarActionPerformed(evt);
+            }
+        });
+
+        JBcadastrar.setBackground(new java.awt.Color(102, 153, 255));
+        JBcadastrar.setFont(new java.awt.Font("Noto Sans", 1, 18)); // NOI18N
+        JBcadastrar.setForeground(new java.awt.Color(255, 255, 255));
+        JBcadastrar.setText("Cadastrar");
+        JBcadastrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBcadastrarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        jLabel1.setText("Nome:");
+
+        jLabel2.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        jLabel2.setText("Marca:");
+
+        jLabel3.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        jLabel3.setText("Custo:");
+
+        JTFnome.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+
+        JTFcusto.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        JTFcusto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFcustoActionPerformed(evt);
+            }
+        });
+
+        jTableFerramenta.setBackground(new java.awt.Color(255, 255, 255));
+        jTableFerramenta.setFont(new java.awt.Font("Noto Sans", 0, 18)); // NOI18N
+        jTableFerramenta.setForeground(new java.awt.Color(0, 0, 0));
+        jTableFerramenta.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "ID", "Nome", "Marca", "Custo"
+            }
+        ));
+        jTableFerramenta.setToolTipText("");
+        jTableFerramenta.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTableFerramentaMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTableFerramenta);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(JBcadastrar)
+                        .addGap(70, 70, 70)
+                        .addComponent(JBalterar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(JBapagar))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 471, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(JTFnome)
+                            .addComponent(JTFmarca)
+                            .addComponent(JTFcusto, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(11, 11, 11)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(JTFnome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(JTFmarca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(JTFcusto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JBcadastrar)
+                    .addComponent(JBalterar)
+                    .addComponent(JBapagar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(JPmenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(JPmenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void JBsairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBsairActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_JBsairActionPerformed
+
+    private void JPmenuMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JPmenuMouseDragged
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+
+        setLocation(x - xMouse, y - yMouse);
+    }//GEN-LAST:event_JPmenuMouseDragged
+
+    private void JPmenuMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JPmenuMousePressed
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }//GEN-LAST:event_JPmenuMousePressed
+
+    private void JBalterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBalterarActionPerformed
+
+    }//GEN-LAST:event_JBalterarActionPerformed
+
+    private void JBapagarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBapagarActionPerformed
+
+    }//GEN-LAST:event_JBapagarActionPerformed
+
+    private void JBcadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBcadastrarActionPerformed
+        
+    }//GEN-LAST:event_JBcadastrarActionPerformed
+
+    private void JTFcustoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFcustoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JTFcustoActionPerformed
+
+    private void jTableFerramentaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableFerramentaMouseClicked
+
+    }//GEN-LAST:event_jTableFerramentaMouseClicked
 
     /**
      * @param args the command line arguments
@@ -78,5 +314,20 @@ public class TelaFerramenta extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JBalterar;
+    private javax.swing.JButton JBapagar;
+    private javax.swing.JButton JBcadastrar;
+    private javax.swing.JButton JBsair;
+    private javax.swing.JPanel JPmenu;
+    private javax.swing.JTextField JTFcusto;
+    private javax.swing.JTextField JTFmarca;
+    private javax.swing.JTextField JTFnome;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTableFerramenta;
     // End of variables declaration//GEN-END:variables
 }
