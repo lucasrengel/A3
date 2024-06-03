db08554bbfcb6d9b6694e087840b2ba8d38d3b5c
isaluch <Isadoraluch@gmail.com>
2024-06-03 18:49:36 -0300

    adicionado: construtor getters e setters

--- src/main/java/modelo/Emprestimo.java
+++ src/main/java/modelo/Emprestimo.java
@@ -17,5 +17,56 @@ public class Emprestimo {
     private Ferramenta idFerramenta;
     private Date dataEmprestimo;
     private Date dataDevolucao;
-    
+
+    public Emprestimo() {
+    }
+
+    public Emprestimo(int id, Amigo idAmigo, Ferramenta idFerramenta, Date dataEmprestimo, Date dataDevolucao) {
+        this.id = id;
+        this.idAmigo = idAmigo;
+        this.idFerramenta = idFerramenta;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Amigo getIdAmigo() {
+        return idAmigo;
+    }
+
+    public void setIdAmigo(Amigo idAmigo) {
+        this.idAmigo = idAmigo;
+    }
+
+    public Ferramenta getIdFerramenta() {
+        return idFerramenta;
+    }
+
+    public void setIdFerramenta(Ferramenta idFerramenta) {
+        this.idFerramenta = idFerramenta;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
 }
