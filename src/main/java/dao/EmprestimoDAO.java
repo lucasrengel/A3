69cec84c5aed9b219444e8577293242f522f3815
PedroEmanueL51027 <brcpedroso5102@gmail.com>
2024-06-04 15:25:07 -0300

    feat: Adiciona metodos crud

--- src/main/java/dao/EmprestimoDAO.java
+++ src/main/java/dao/EmprestimoDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import modelo.Amigo;
 import modelo.Emprestimo;
@@ -48,6 +50,84 @@ public class EmprestimoDAO {
         }
 
         return MinhaLista;
+    }
+
+    public void registrarEmprestimo(Emprestimo emprestimo) {
+        String sql = "INSERT INTO tb_emprestimos (id_ferramenta, id_amigo, data_emprestimo, data_devolucao) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, emprestimo.getIdAmigo().getId());
+            stmt.setInt(2, emprestimo.getIdFerramenta().getId());
+            stmt.setDate(3, emprestimo.getDataEmprestimo());
+            stmt.setDate(4, emprestimo.getDataDevolucao());
+
+            stmt.execute();
+            stmt.close();
+
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean deleteEmprestimoBD(int id) {
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            stmt.executeUpdate("DELETE FROM tb_emprestimos WHERE id = " + id);
+            stmt.close();
+
+        } catch (SQLException erro) {
+        }
+
+        return true;
+    }
+
+    public boolean updateEmprestimoBD(Emprestimo emprestimo) {
+
+        String sql = "UPDATE tb_emprestimos set id_ferramenta = ? ,id_amigo = ? ,data_emprestimo = ?, data_devolucao = ? WHERE id = ?";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, emprestimo.getIdFerramenta().getId());
+            stmt.setInt(2, emprestimo.getIdAmigo().getId());
+            stmt.setDate(3, emprestimo.getDataEmprestimo());
+            stmt.setDate(4, emprestimo.getDataDevolucao());
+            stmt.setInt(5, emprestimo.getId());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
+        }
+
+    }
+
+    public Emprestimo carregaEmprestimo(int id) {
+
+        Emprestimo emprestimo = new Emprestimo();
+        emprestimo.setId(id);
+
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_emprestimos WHERE id = " + id);
+            res.next();
+
+            emprestimo.setIdAmigo(amigoDAO.carregaAmigo(res.getInt("id_amigo")));
+            emprestimo.setIdFerramenta(ferramentaDAO.carregaFerramenta(res.getInt("id_ferramenta")));
+            emprestimo.setDataEmprestimo(res.getDate("data_emprestimo"));
+            emprestimo.setDataDevolucao(res.getDate("data_devolucao"));
+
+            stmt.close();
+
+        } catch (SQLException erro) {
+        }
+        return emprestimo;
     }
 
     private Connection getConexao() {
